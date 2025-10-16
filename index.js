8c7b1d23007da9f4ce4a3d8c0980a86bcca07345
marius-stack <efuetlanchamarius@gmail.com>
2025-10-16 17:52:48 +0100

    all files modified

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ let guestRender = document.querySelector("#render-guest");
 let saveEl = document.querySelector("#save");
 let resetEl = document.querySelector("#reset");
 let scoreEl = document.querySelector("#score");
-
+let winnerEl = document.querySelector("#winner");
+// console.log(resetEl);
 let result = {
   count: 0,
   count2: 0,
 };
 
+function reset() {
+  let restart = 0;
+  if (homeRender.textContent !== 0) {
+    result.count = restart;
+  } else if (guestRender.textContent !== 0) {
+    result.count2 = restart;
+  }
+}
+
 function addH() {
   homeRender.textContent = result.count++;
+  updateScoreEl();
 }
 
 function addTwoH() {
@@ -35,5 +46,18 @@ function addThreeG() {
 
 function save() {
   scoreEl.innerText = homeRender.textContent + "-" + guestRender.textContent;
+  updateScoreEl();
   reset();
+}
+
+function updateScoreEl() {
+  let score1 = "Home!";
+  let score2 = "Guest!";
+  if (homeRender.textContent > guestRender.textContent) {
+    winnerEl.innerText = "current Winner: " + score1;
+  } else if (homeRender.textContent < guestRender.textContent) {
+    winnerEl.innerText = "current Winner: " + score2;
+  } else if (homeRender.textContent === guestRender.textContent) {
+    winnerEl.innerText = "It's a Draw";
+  }
 }
